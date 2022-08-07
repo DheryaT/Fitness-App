0bafacc734634e8189828e2eff844688511d0ab7
Dherya <dtailorvg@gmail.com>
2022-08-07 16:28:08 +1200

    Nav test

---
+++ gym-app/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text} from "react-native";
+
+const Home = () => {
+    return(
+        <View style={{backgroundColor:'red'}}>
+            <Text>Home!</Text>
+        </View>
+    )
+}
+
+
+export { Home };
---
+++ gym-app/components/Tabs.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Home } from "./Home";
+import { Calculator } from "./Calculator";
+
+
+const Tabs = () => {
+
+    const Tab = createBottomTabNavigator();
+
+    return(
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={Home} />
+                <Tab.Screen name="Calc" component={Calculator} />
+            </Tab.Navigator>
+            <Text>he</Text>
+        </NavigationContainer>
+    )
+}
+
+export { Tabs };
