b8c3a41ba35109b2189beb072616c2f95b337e40
Zhoualle <zhoualle@myvuw.ac.nz>
2022-08-24 23:22:09 +1200

    Timer times added

--- gym-app/components/Tabs.jsx
+++ gym-app/components/Tabs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, Button } from "react-native";
+import { StyleSheet, Text, Button, TouchableOpacity, Image } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home } from "./Home";
@@ -11,44 +11,61 @@ import { logoutFunction } from "../api/Authentication";
 
 const Tabs = (props) => {
 
-    const Tab = createBottomTabNavigator();
+  const Tab = createBottomTabNavigator();
 
-    return(
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={Home} initialParams = {{user: props.user}}  options={{
-                     headerTitle: "Home",
-                     headerRight: () => (
-                     <Button
-                        onPress={() => logoutFunction(props.auth)}
-                        title="LogOut"
-                        color="black"
-                        />
-                      ),
-                    }}/>
-                <Tab.Screen name="Calc" component={Calculator} options={{
-                     headerTitle: "Home",
-                     headerRight: () => (
-                     <Button
-                        onPress={() => logoutFunction(props.auth)}
-                        title="LogOut"
-                        color="black"
-                        />
-                      ),
-                    }}/>
-                <Tab.Screen name="Timer" component={Timer} options={{
-                     headerTitle: "Home",
-                     headerRight: () => (
-                     <Button
-                        onPress={() => logoutFunction(props.auth)}
-                        title="LogOut"
-                        color="black"
-                        />
-                      ),
-                    }}/>
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} initialParams={{ user: props.user }} options={{
+          headerTitle: "Home",
+          headerRight: () => (
+            <Button
+              onPress={() => logoutFunction(props.auth)}
+              title="LogOut"
+              color="black"
+            />
+          ),
+        }} />
+        <Tab.Screen name="Calc" component={Calculator} options={{
+          headerTitle: "Home",
+          headerRight: () => (
+            <Button
+              onPress={() => logoutFunction(props.auth)}
+              title="LogOut"
+              color="black"
+            />
+          ),
+        }} />
+        <Tab.Screen name="Timer" component={Timer} options={{
+          headerTitle: "Timer",
+          headerRight: () => (
+            <Button
+              onPress={() => logoutFunction(props.auth)}
+              title="LogOut"
+              color="black"
+            />
+          ),
+          headerLeft: () => (
+            <TouchableOpacity style={styles.buttonLoc} onPress={() => { alert("Timer does this and does that") }}>
+              <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Icon-round-Question_mark.svg/640px-Icon-round-Question_mark.svg.png', }}
+                style={styles.image1} />
+            </TouchableOpacity>
+          ),
+        }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
-
+const styles = StyleSheet.create({
+  image1: {
+    height: 30,
+    width: 30,
+    marginLeft: 30,
+    marginTop: -10,
+  },
+  buttonLoc: {
+    height: "30%",
+    width: "30%",
+  },
+})
 export { Tabs };
--- gym-app/components/Timer.jsx
+++ gym-app/components/Timer.jsx
@@ -1,13 +1,122 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from 'react';
+import CountDown from 'react-native-countdown-component';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
+
 
 const Timer = () => {
-    return(
-        <View>
-            <Text>Timer</Text>
-        </View>
+
+    return (
+        <SafeAreaView style={styles.container}>
+
+            <View style={styles.quickhead}>
+
+                <Text style={styles.textStyle}>Quick Start:</Text >
+            </View>
+
+            <Text style={styles.textSets}>Prepare: <CountDown
+                style={styles.TimerS}
+                size={15}
+                timeToShow={['M', 'S']}
+                //onFinish={() => alert('finished')}
+                onPress={() => alert('Timer started')}
+                timeLabels={{ m: 'min', s: 'sec' }}
+                showSeparator
+                digitStyle={{
+                    backgroundColor: '#e0e0e0',
+                    margintop: 20,
+                }}
+            />
+            </Text >
+            <Text style={styles.textSets}>Sets:    <CountDown
+                style={styles.TimerS}
+                size={15}
+
+                timeToShow={['M', 'S']}
+                //onFinish={() => alert('finished')}
+                onPress={() => alert('Timer started')}
+                showSeparator
+                timeLabels={{ m: 'min', s: 'sec' }}
+                digitStyle={{
+                    backgroundColor: '#e0e0e0',
+                    margintop: 20,
+                }}
+            /></Text >
+            <Text style={styles.textSets}>Work:    <CountDown
+                style={styles.TimerS}
+                size={15}
+
+                timeToShow={['M', 'S']}
+                //onFinish={() => alert('finished')}
+                onPress={() => alert('Timer started')}
+                timeLabels={{ m: 'min', s: 'sec' }}
+                showSeparator
+                digitStyle={{
+                    backgroundColor: '#e0e0e0',
+                    margintop: 20,
+                }}
+            /></Text >
+            <Text style={styles.textSets}>Rest:    <CountDown
+                style={styles.TimerS}
+                size={15}
+
+                timeToShow={['M', 'S']}
+                //onFinish={() => alert('finished')}
+                onPress={() => alert('Timer started')}
+                showSeparator
+                timeLabels={{ m: 'min', s: 'sec' }}
+                digitStyle={{
+                    backgroundColor: '#e0e0e0',
+                    margintop: 20,
+                }}
+            /></Text >
+            <Text style={styles.textSets}>Cooldown:<CountDown
+                style={styles.TimerS}
+                size={15}
+
+                timeToShow={['M', 'S']}
+                //onFinish={() => alert('finished')}
+                onPress={() => alert('Timer started')}
+                timeLabels={{ m: 'min', s: 'sec' }}
+                showSeparator
+
+                digitStyle={{
+                    backgroundColor: '#e0e0e0',
+                    margintop: 20,
+                }}
+            />
+            </Text >
+
+
+        </SafeAreaView>
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#e0e0e0',
+        flex: 1,
+    },
+    quickhead: {
+        backgroundColor: '#D3D3D3',
+        maxHeight: "10%",
+        flex: 1,
+        alignItems: "left",
+        justifyContent: "center"
 
-export { Timer }; //tes
+    },
+
+    textStyle: {
+        fontSize: 30,
+        marginLeft: 30,
+    },
+    textSets: {
+        fontSize: 20,
+        marginLeft: 30,
+    },
+    TimerS: {
+        marginRight: 50,
+        alignItems: 'center'
+
+    }
+})
+export { Timer }; 
