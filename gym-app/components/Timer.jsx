41ed10af40483d2bc6a348df2b721c27bae9c404
Zhoualle <zhoualle@myvuw.ac.nz>
2022-08-25 00:08:44 +1200

    Timers lined up and added scroll view

--- gym-app/components/Timer.jsx
+++ gym-app/components/Timer.jsx
@@ -6,14 +6,15 @@ import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ScrollView } fr
 const Timer = () => {
 
     return (
-        <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
 
             <View style={styles.quickhead}>
 
                 <Text style={styles.textStyle}>Quick Start:</Text >
             </View>
 
-            <Text style={styles.textSets}>Prepare: <CountDown
+            <Text style={styles.textSets}>Prepare: {"\n"} {"\n"} {"\n"}Sets:{"\n"} {"\n"} {"\n"}Work:{"\n"} {"\n"} {"\n"}Rest:{"\n"} {"\n"} {"\n"}Cooldown:</Text >
+            <CountDown
                 style={styles.TimerS}
                 size={15}
                 timeToShow={['M', 'S']}
@@ -26,8 +27,7 @@ const Timer = () => {
                     margintop: 20,
                 }}
             />
-            </Text >
-            <Text style={styles.textSets}>Sets:    <CountDown
+            <CountDown
                 style={styles.TimerS}
                 size={15}
 
@@ -36,58 +36,56 @@ const Timer = () => {
                 onPress={() => alert('Timer started')}
                 showSeparator
                 timeLabels={{ m: 'min', s: 'sec' }}
-                digitStyle={{
-                    backgroundColor: '#e0e0e0',
-                    margintop: 20,
-                }}
-            /></Text >
-            <Text style={styles.textSets}>Work:    <CountDown
-                style={styles.TimerS}
-                size={15}
-
-                timeToShow={['M', 'S']}
-                //onFinish={() => alert('finished')}
-                onPress={() => alert('Timer started')}
-                timeLabels={{ m: 'min', s: 'sec' }}
-                showSeparator
-                digitStyle={{
-                    backgroundColor: '#e0e0e0',
-                    margintop: 20,
-                }}
-            /></Text >
-            <Text style={styles.textSets}>Rest:    <CountDown
-                style={styles.TimerS}
-                size={15}
-
-                timeToShow={['M', 'S']}
-                //onFinish={() => alert('finished')}
-                onPress={() => alert('Timer started')}
-                showSeparator
-                timeLabels={{ m: 'min', s: 'sec' }}
-                digitStyle={{
-                    backgroundColor: '#e0e0e0',
-                    margintop: 20,
-                }}
-            /></Text >
-            <Text style={styles.textSets}>Cooldown:<CountDown
-                style={styles.TimerS}
-                size={15}
-
-                timeToShow={['M', 'S']}
-                //onFinish={() => alert('finished')}
-                onPress={() => alert('Timer started')}
-                timeLabels={{ m: 'min', s: 'sec' }}
-                showSeparator
-
                 digitStyle={{
                     backgroundColor: '#e0e0e0',
                     margintop: 20,
                 }}
             />
-            </Text >
+            <CountDown
+                style={styles.TimerS}
+                size={15}
 
+                timeToShow={['M', 'S']}
+                //onFinish={() => alert('finished')}
+                onPress={() => alert('Timer started')}
+                showSeparator
+                timeLabels={{ m: 'min', s: 'sec' }}
+                digitStyle={{
+                    backgroundColor: '#e0e0e0',
+                    margintop: 20,
+                }}
+            />
+            <CountDown
+                style={styles.TimerS}
+                size={15}
 
-        </SafeAreaView>
+                timeToShow={['M', 'S']}
+                //onFinish={() => alert('finished')}
+                onPress={() => alert('Timer started')}
+                showSeparator
+                timeLabels={{ m: 'min', s: 'sec' }}
+                digitStyle={{
+                    backgroundColor: '#e0e0e0',
+                    margintop: 20,
+                }}
+            />
+
+            <CountDown
+                style={styles.TimerS}
+                size={15}
+
+                timeToShow={['M', 'S']}
+                //onFinish={() => alert('finished')}
+                onPress={() => alert('Timer started')}
+                showSeparator
+                timeLabels={{ m: 'min', s: 'sec' }}
+                digitStyle={{
+                    backgroundColor: '#e0e0e0',
+                    margintop: 20,
+                }}
+            />
+
+        </ScrollView>
     )
 }
 
@@ -98,8 +96,8 @@ const styles = StyleSheet.create({
     },
     quickhead: {
         backgroundColor: '#D3D3D3',
-        maxHeight: "10%",
-        flex: 1,
+        padding:"5%",
+        
         alignItems: "left",
         justifyContent: "center"
 
@@ -107,15 +105,18 @@ const styles = StyleSheet.create({
 
     textStyle: {
         fontSize: 30,
-        marginLeft: 30,
+        marginLeft: "5%",
     },
     textSets: {
         fontSize: 20,
-        marginLeft: 30,
+        marginLeft: "10%",
+        marginTop:"28%",
+        position: 'absolute',
+
     },
     TimerS: {
-        marginRight: 50,
-        alignItems: 'center'
+        marginLeft: "20%",
+        marginTop: "4%",
 
     }
 })
