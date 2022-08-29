2c8c2d9397c28d49ef1dc178aeb2ef6b9e9ab3d6
Zhoualle <zhoualle@myvuw.ac.nz>
2022-08-29 16:57:37 +1200

    Changed icons to Fa awesome icons

--- gym-app/components/Timer.jsx
+++ gym-app/components/Timer.jsx
@@ -8,6 +8,11 @@ import { Tabs } from "./Tabs";
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons/faCirclePlay'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons/faBookmark'
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons/faCirclePlus'
+import { faCircleMinus } from '@fortawesome/free-solid-svg-icons/faCircleMinus'
+
+
 
 
 const Timer = ({ navigation }) => {
@@ -37,8 +42,8 @@ const Timer = ({ navigation }) => {
             <View style={styles.body}>
                 <Text style={styles.textSets}>Prepare:{"\n"} {"\n"} {"\n"}{"\n"}Sets:{"\n"} {"\n"}{"\n"} {"\n"}Work:{"\n"} {"\n"} {"\n"}{"\n"}Rest:{"\n"} {"\n"} {"\n"}{"\n"}Cooldown:</Text >
 
-                <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/659/659892.png', }} style={styles.image1} /></TouchableOpacity>
-                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/262/262038.png', }} style={styles.image2} /></TouchableOpacity>
+                <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><FontAwesomeIcon icon={faCircleMinus} size={30}/></TouchableOpacity>
+                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><FontAwesomeIcon icon={faCirclePlus} size={30}/></TouchableOpacity>
 
                 <CountDown
 
@@ -57,9 +62,9 @@ const Timer = ({ navigation }) => {
                     }}
 
                 />
-                <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/659/659892.png', }} style={styles.image1} /></TouchableOpacity>
-                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/262/262038.png', }} style={styles.image2} /></TouchableOpacity>
-
+               
+               <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><FontAwesomeIcon icon={faCircleMinus} size={30}/></TouchableOpacity>
+                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><FontAwesomeIcon icon={faCirclePlus} size={30}/></TouchableOpacity>
                 <CountDown
                     style={styles.TimerS}
                     size={15}
@@ -74,9 +79,9 @@ const Timer = ({ navigation }) => {
 
                     }}
                 />
-                <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/659/659892.png', }} style={styles.image1} /></TouchableOpacity>
-                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/262/262038.png', }} style={styles.image2} /></TouchableOpacity>
-
+           
+           <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><FontAwesomeIcon icon={faCircleMinus} size={30}/></TouchableOpacity>
+                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><FontAwesomeIcon icon={faCirclePlus} size={30}/></TouchableOpacity>
                 <CountDown
                     style={styles.TimerS}
                     size={15}
@@ -91,8 +96,9 @@ const Timer = ({ navigation }) => {
 
                     }}
                 />
-                <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/659/659892.png', }} style={styles.image1} /></TouchableOpacity>
-                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/262/262038.png', }} style={styles.image2} /></TouchableOpacity>
+           
+           <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><FontAwesomeIcon icon={faCircleMinus} size={30}/></TouchableOpacity>
+                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><FontAwesomeIcon icon={faCirclePlus} size={30}/></TouchableOpacity>
 
                 <CountDown
                     style={styles.TimerS}
@@ -111,8 +117,9 @@ const Timer = ({ navigation }) => {
                     }}
                 />
 
-                <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/659/659892.png', }} style={styles.image1} /></TouchableOpacity>
-                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/262/262038.png', }} style={styles.image2} /></TouchableOpacity>
+                
+<TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><FontAwesomeIcon icon={faCircleMinus} size={30}/></TouchableOpacity>
+                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><FontAwesomeIcon icon={faCirclePlus} size={30}/></TouchableOpacity>
 
                 <CountDown
                     style={styles.TimerS}
@@ -134,6 +141,8 @@ const Timer = ({ navigation }) => {
             <View style={styles.container1}>
             <TouchableOpacity style={styles.startBut}  onPress={() => navigation.navigate('CountdownTimer')} ><FontAwesomeIcon icon={faCirclePlay} size={50}/></TouchableOpacity>
                     <Text style={styles.textStart}>Start</Text>
+                    <TouchableOpacity style={styles.saveBut}  onPress={() => {alert("save preset")}} ><FontAwesomeIcon icon={faBookmark} size={50}/></TouchableOpacity>
+                    <Text style={styles.textSave}>Bookmark</Text>
             </View>
             <View style={styles.quickhead}>
                     <Text style={styles.textStyle}> Presets</Text>
@@ -176,22 +185,28 @@ const styles = StyleSheet.create({
         width: 25,
         height: 25,
         top: "12%",
-        left: "42%",
+        left: "45%",
     },
     image2: {
         width: 25,
         height: 25,
         top: "7%",
-        left: "78%",
+        left: "85%",
     },
 
     textStyle: {
         fontSize: 30,
         marginLeft: "5%",
     },
+    textSave: {
+        fontSize: 20,
+        left: '70%',
+        bottom:'30%',
+    },
     textStart: {
         fontSize: 20,
         left: '11%',
+        top:"20%"
     },
     textSets: {
         fontSize: 23,
@@ -202,11 +217,15 @@ const styles = StyleSheet.create({
 
     },
     TimerS: {
-        marginLeft: "35%",
+        left: "20%",
     },
     startBut: {
-        
         left: '10%',
+        top:"20%"
+    },    
+    saveBut: {
+        bottom: '30%',
+        left: '75%',
     }
 })
 export { Timer }; 
