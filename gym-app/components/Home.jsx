05f265723fc27f584c1bde2b1db42c4cf2b3a856
Dherya <dtailorvg@gmail.com>
2022-09-02 18:26:02 +1200

    Database test

--- gym-app/components/Home.jsx
+++ gym-app/components/Home.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import {ImageBackground, StyleSheet,Text,View, Image,TextInput} from 'react-native';
+import {ImageBackground, StyleSheet,Text,View, Image,TextInput, Button} from 'react-native';
+import { doc, setDoc } from "firebase/firestore"; 
+import { auth, db } from "../firebase-config";
 
 const Home = ({navigation, route}) => {
+
+    const sendUser = async () => {
+        await setDoc(doc(db, "users", `${route.params.user?.email}`), {
+            name: `${route.params.user?.email}`,
+            sched: { day: "back"}
+        })
+    }
+
     return(
         <View style={styles.container}>
             <Text style={styles.Title}>Hi {route.params.user?.email} {"\n"}Let's check your activity</Text>
+            <Button title= "Hello" onPress={sendUser}></Button>
         </View>
     )
 }
