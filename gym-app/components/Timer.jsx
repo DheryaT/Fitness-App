b3aa9bf989cab15d0e80f9d55711e36acbf7e3c2
Zhoualle <zhoualle@myvuw.ac.nz>
2022-09-02 21:31:17 +1200

    fixed countdown

--- gym-app/components/Timer.jsx
+++ gym-app/components/Timer.jsx
@@ -1,156 +1,151 @@
 import React, { useState, useEffect } from 'react';
 import CountDown from 'react-native-countdown-component';
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ScrollView, Image, Button } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { CountdownTimer } from "./CountdownTimer";
-import { Tabs } from "./Tabs";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Image, Button,Pressable,TouchableOpacity } from 'react-native';
 
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faCirclePlay } from '@fortawesome/free-solid-svg-icons/faCirclePlay'
-import { faBookmark } from '@fortawesome/free-solid-svg-icons/faBookmark'
-import { faCirclePlus } from '@fortawesome/free-solid-svg-icons/faCirclePlus'
-import { faCircleMinus } from '@fortawesome/free-solid-svg-icons/faCircleMinus'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons/faCirclePlay';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons/faBookmark';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons/faCirclePlus';
+import { faCircleMinus } from '@fortawesome/free-solid-svg-icons/faCircleMinus';
 
 
 
 
-const Timer = ({ navigation }) => {
+export default function Timer({ navigation }){
     // State to store count value
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
     const incrementCount = () => {
         setCount(count + 1);
-    };
+    }
     const DecrementCount = () => {
+        if(count>0){
         setCount(count - 1);
-    };
-    const Stacks = createNativeStackNavigator();
+        }
+        else{
+            setCount(0);
+        }
+    }
 
     return (
 
 
         <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={{ flexGrow: 1 }}>
+            <ScrollView style={styles.container} contentContainerStyle={{ flexGrow: 1 }}>
 
 
 
-            <View style={styles.quickhead}>
-                <Text style={styles.textStyle}>Quick Start:</Text >
-            </View>
+                <View style={styles.quickhead}>
+                    <Text style={styles.textStyle}>Quick Start:</Text >
+                </View>
 
 
-            <View style={styles.body}>
-                <Text style={styles.textSets}>Prepare:{"\n"} {"\n"} {"\n"}{"\n"}Sets:{"\n"} {"\n"}{"\n"} {"\n"}Work:{"\n"} {"\n"} {"\n"}{"\n"}Rest:{"\n"} {"\n"} {"\n"}{"\n"}Cooldown:</Text >
+                <View style={styles.body}>
+                    <Text style={styles.textSets}>Prepare:{"\n"} {"\n"} {"\n"}{"\n"}Sets:{"\n"} {"\n"}{"\n"} {"\n"}Work:{"\n"} {"\n"} {"\n"}{"\n"}Rest:{"\n"} {"\n"} {"\n"}{"\n"}Cooldown:</Text >
 
-                <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><FontAwesomeIcon icon={faCircleMinus} size={30}/></TouchableOpacity>
-                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><FontAwesomeIcon icon={faCirclePlus} size={30}/></TouchableOpacity>
+                    <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount()}}><FontAwesomeIcon icon={faCircleMinus} size={30} /></TouchableOpacity>
+                    <TouchableOpacity style={styles.image2} onPress={() => { incrementCount() }}><FontAwesomeIcon icon={faCirclePlus} size={30} /></TouchableOpacity>
 
-                <CountDown
+                    <CountDown
 
-                    style={styles.TimerS}
-                    size={15}
-                    timeToShow={['M', 'S']}
-                    //onFinish={() => alert('finished')}
-                    //onPress={() => alert('Timer started')}
-                    timeLabels={{ m: 'min', s: 'sec' }}
-                    showSeparator
-                    until={count}
+                        style={styles.TimerS}
+                        size={15}
+                        timeToShow={['M', 'S']}
+                        timeLabels={{ m: 'min', s: 'sec' }}
+                        showSeparator
+                        running={false}
+                        until={count}
 
-                    digitStyle={{
-                        backgroundColor: '#e0e0e0',
+                        digitStyle={{
+                            backgroundColor: '#e0e0e0',
 
-                    }}
+                        }}
 
-                />
-               
-               <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><FontAwesomeIcon icon={faCircleMinus} size={30}/></TouchableOpacity>
-                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><FontAwesomeIcon icon={faCirclePlus} size={30}/></TouchableOpacity>
-                <CountDown
-                    style={styles.TimerS}
-                    size={15}
-                    timeToShow={['M', 'S']}
-                    //onFinish={() => alert('finished')}
-                    //onPress={() => alert('Timer started')}
-                    showSeparator
-                    timeLabels={{ m: 'min', s: 'sec' }}
-                    until={count}
-                    digitStyle={{
-                        backgroundColor: '#e0e0e0',
+                    />
 
-                    }}
-                />
-           
-           <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><FontAwesomeIcon icon={faCircleMinus} size={30}/></TouchableOpacity>
-                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><FontAwesomeIcon icon={faCirclePlus} size={30}/></TouchableOpacity>
-                <CountDown
-                    style={styles.TimerS}
-                    size={15}
-                    timeToShow={['M', 'S']}
-                    //onFinish={() => alert('finished')}
-                    //onPress={() => alert('Timer started')}
-                    showSeparator
-                    timeLabels={{ m: 'min', s: 'sec' }}
-                    until={count}
-                    digitStyle={{
-                        backgroundColor: '#e0e0e0',
+                    <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount() }}><FontAwesomeIcon icon={faCircleMinus} size={30} /></TouchableOpacity>
+                    <TouchableOpacity style={styles.image2} onPress={() => { incrementCount() }}><FontAwesomeIcon icon={faCirclePlus} size={30} /></TouchableOpacity>
+                    <CountDown
+                        style={styles.TimerS}
+                        size={15}
+                        timeToShow={['M', 'S']}
+                        showSeparator
+                        timeLabels={{ m: 'min', s: 'sec' }}
+                        running={false}
+                        until={count}
+                        digitStyle={{
+                            backgroundColor: '#e0e0e0',
 
-                    }}
-                />
-           
-           <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><FontAwesomeIcon icon={faCircleMinus} size={30}/></TouchableOpacity>
-                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><FontAwesomeIcon icon={faCirclePlus} size={30}/></TouchableOpacity>
+                        }}
+                    />
 
-                <CountDown
-                    style={styles.TimerS}
-                    size={15}
+                    <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount() }}><FontAwesomeIcon icon={faCircleMinus} size={30} /></TouchableOpacity>
+                    <TouchableOpacity style={styles.image2} onPress={() => { incrementCount() }}><FontAwesomeIcon icon={faCirclePlus} size={30} /></TouchableOpacity>
+                    <CountDown
+                        style={styles.TimerS}
+                        size={15}
+                        timeToShow={['M', 'S']}
+                        showSeparator
+                        timeLabels={{ m: 'min', s: 'sec' }}
+                        running={false}
+                        until={count}
+                        digitStyle={{
+                            backgroundColor: '#e0e0e0',
 
-                    timeToShow={['M', 'S']}
-                    //onFinish={() => alert('finished')}
-                    //onPress={() => alert('Timer started')}
-                    showSeparator
-                    timeLabels={{ m: 'min', s: 'sec' }}
-                    until={count}
-                    digitStyle={{
-                        backgroundColor: '#e0e0e0',
-                        borderColor: "#fff",
+                        }}
+                    />
 
-                    }}
-                />
+                    <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount() }}><FontAwesomeIcon icon={faCircleMinus} size={30} /></TouchableOpacity>
+                    <TouchableOpacity style={styles.image2} onPress={() => { incrementCount() }}><FontAwesomeIcon icon={faCirclePlus} size={30} /></TouchableOpacity>
 
-                
-<TouchableOpacity style={styles.image1} onPress={() => { DecrementCount }}><FontAwesomeIcon icon={faCircleMinus} size={30}/></TouchableOpacity>
-                <TouchableOpacity style={styles.image2} onPress={() => { incrementCount }}><FontAwesomeIcon icon={faCirclePlus} size={30}/></TouchableOpacity>
+                    <CountDown
+                        style={styles.TimerS}
+                        size={15}
 
-                <CountDown
-                    style={styles.TimerS}
-                    size={15}
+                        timeToShow={['M', 'S']}
+                        showSeparator
+                        timeLabels={{ m: 'min', s: 'sec' }}
+                        running={false}
+                        until={count}
+                        digitStyle={{
+                            backgroundColor: '#e0e0e0',
+                            borderColor: "#fff",
 
-                    timeToShow={['M', 'S']}
-                    //onFinish={() => alert('finished')}
-                    //onPress={() => alert('Timer started')}
-                    showSeparator
-                    timeLabels={{ m: 'min', s: 'sec' }}
-                    until={count}
-                    digitStyle={{
-                        backgroundColor: '#e0e0e0',
+                        }}
+                    />
 
-                    }}
-                />
 
-            </View>
-            <View style={styles.container1}>
-            <TouchableOpacity style={styles.startBut}  onPress={() => navigation.navigate('CountdownTimer')} ><FontAwesomeIcon icon={faCirclePlay} size={50}/></TouchableOpacity>
+                    <TouchableOpacity style={styles.image1} onPress={() => { DecrementCount() }}><FontAwesomeIcon icon={faCircleMinus} size={30} /></TouchableOpacity>
+                    <TouchableOpacity style={styles.image2} onPress={() => { incrementCount() }}><FontAwesomeIcon icon={faCirclePlus} size={30} /></TouchableOpacity>
+
+                    <CountDown
+                        style={styles.TimerS}
+                        size={15}
+
+                        timeToShow={['M', 'S']}
+                        showSeparator
+                        timeLabels={{ m: 'min', s: 'sec' }}
+                        running={false}
+                        until={count}
+                        digitStyle={{
+                            backgroundColor: '#e0e0e0',
+
+                        }}
+                    />
+
+                </View>
+                <View style={styles.container1}>
+                    <TouchableOpacity style={styles.startBut} onPress={() => navigation.navigate('CountdownTimer')} ><FontAwesomeIcon icon={faCirclePlay} size={50} /></TouchableOpacity>
                     <Text style={styles.textStart}>Start</Text>
-                    <TouchableOpacity style={styles.saveBut}  onPress={() => {alert("save preset")}} ><FontAwesomeIcon icon={faBookmark} size={50}/></TouchableOpacity>
+                    <TouchableOpacity style={styles.saveBut} onPress={() => { alert("save preset") }} ><FontAwesomeIcon icon={faBookmark} size={50} /></TouchableOpacity>
                     <Text style={styles.textSave}>Bookmark</Text>
-            </View>
-            <View style={styles.quickhead}>
+                </View>
+                <View style={styles.quickhead}>
                     <Text style={styles.textStyle}> Presets</Text>
-            </View>
+                </View>
 
 
-        </ScrollView>
-            </SafeAreaView>
+            </ScrollView>
+        </SafeAreaView>
 
     )
 }
@@ -159,17 +154,17 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#e0e0e0',
         flex: 1,
-        
-        
+
+
     },
     container1: {
         backgroundColor: '#e0e0e0',
         flex: 1,
         margin: 30,
         borderColor: '#000000',
-        
+
     },
-    
+
     body: {
         backgroundColor: '#e0e0e0',
         flex: 1,
@@ -184,12 +179,14 @@ const styles = StyleSheet.create({
     image1: {
         width: 25,
         height: 25,
+        zIndex:1,
         top: "12%",
         left: "45%",
     },
     image2: {
         width: 25,
         height: 25,
+        zIndex:1,
         top: "7%",
         left: "85%",
     },
@@ -201,19 +198,19 @@ const styles = StyleSheet.create({
     textSave: {
         fontSize: 20,
         left: '70%',
-        bottom:'30%',
+        bottom: '30%',
     },
     textStart: {
         fontSize: 20,
         left: '11%',
-        top:"20%"
+        top: "20%"
     },
     textSets: {
         fontSize: 23,
         marginLeft: "10%",
         marginTop: "14%",
         position: 'absolute',
-        
+
 
     },
     TimerS: {
@@ -221,8 +218,8 @@ const styles = StyleSheet.create({
     },
     startBut: {
         left: '10%',
-        top:"20%"
-    },    
+        top: "20%"
+    },
     saveBut: {
         bottom: '30%',
         left: '75%',
