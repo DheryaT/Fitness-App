1a9d73e019131365fca8610f8273b817274220b4
zhoualle <zhoualle@myvuw.ac.nz>
2022-09-09 14:28:14 +1200

    Home page step counter

--- gym-app/components/Home.jsx
+++ gym-app/components/Home.jsx
@@ -1,34 +1,36 @@
 import React from "react";
-import {ImageBackground, StyleSheet,Text,View, Image,TextInput, Button} from 'react-native';
+import {ImageBackground, StyleSheet,Text,View, Image,TextInput, Button, ComponentProvider} from 'react-native';
 import { doc, setDoc } from "firebase/firestore"; 
 import { auth, db } from "../firebase-config";
 import { useState } from "react";
+import { Pedometer } from 'expo-sensors';
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const Home = ({navigation, route}) => {
 
-    const [number, setNumber] = useState(0);
+    const [pastStepCount, setNumber] = useState(0);
     
-    const updateNum = () => {
-        setNumber(number + 1)
-    }
-    
-
-    const sendUser = async () => {
-        await setDoc(doc(db, "users", `${route.params.user?.email}`), 
-        {
-            schedule: 
-            [{id: 1, name: "back", workout:[{sets: '1', exercise: 'rows', reps: '12'}, {sets: '3', exercise: 'pull ups', reps: '12'}, {sets: '3', exercise: 'pull ups', reps: '12'}, {sets: '3', exercise: 'pull ups', reps: '12'},{sets: '3', exercise: 'pull ups', reps: '12'}]}, {id: 2, name: "back", workout:[{sets: '1', exercise: 'rows', reps: '12'}]}]
-
-        },
-        {merge: true}
-        )
-    }
+    const end = new Date();
+    const start = new Date();
+    start.setDate(end.getDate() - 1);
+    Pedometer.getStepCountAsync(start, end).then(
+      result => {
+        setNumber(result.steps);
+      },
+    );
+  
 
     return(
         <View style={styles.container}>
-            <Text style={styles.Title}>Hi {route.params.user?.email} {"\n"}Let's check your activity {number}</Text>
-            <Button title= "Hello" onPress={sendUser}></Button>
-            <Button title= "update" onPress={updateNum}></Button>
+                        <Text style={styles.Title}>Hi {route.params.user?.email} </Text>
+
+        <View style={styles.steps}>
+
+            <Text style={styles.stepText}>Your Activity:</Text>
+            <Text style={styles.stepText}>Steps: {pastStepCount}</Text>
+
+            </View>
+
         </View>
     )
 }
@@ -36,10 +38,27 @@ const Home = ({navigation, route}) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        margin: 20
+        alignItems:"center",
+        padding: 20,
+        backgroundColor: 'rgb(77, 77, 77)',
+
+
+    },
+    steps: {
+        alignItems:"center",
+        padding: 30,
+        backgroundColor: "#D3D3D3",
+        borderWidth:3,
+        borderRadius:20,
+        borderColor:'white'
+
     },
     Title:{
         fontSize: 22
+    },
+    stepText:{
+        fontSize: 30,
+        color:'white'
     },
 
 })
