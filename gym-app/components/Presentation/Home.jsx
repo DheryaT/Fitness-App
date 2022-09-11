1b1878f8a36fb1deee43c38dec82f7cce4453278
Zhoualle <zhoualle@myvuw.ac.nz>
2022-09-11 15:40:06 +1200

    fixed error

--- gym-app/components/Presentation/Home.jsx
+++ gym-app/components/Presentation/Home.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View,} from 'react-native';
-import { doc, setDoc } from "firebase/firestore";
 import { auth, db } from "../../firebase-config";
 import { useState } from "react";
 import { Pedometer } from 'expo-sensors';
-import { Colors } from "react-native/Libraries/NewAppScreen";
 import { faPersonWalking } from '@fortawesome/free-solid-svg-icons/faPersonWalking';
 import { faHand } from '@fortawesome/free-solid-svg-icons/faHand';
 
